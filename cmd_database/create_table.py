102b47aeb68d0912c50c079c98719fa81096f9ee
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-10-17 13:57:33 +0300

    ToDo

--- cmd_database/create_table.py
+++
@@ -1,8 +0,0 @@
-from To_Do import app, db
-from To_Do.models import User, TaskCollaborator, Task, Note
-
-# Create an application context
-with app.app_context():
-    # Create all tables
-    db.create_all()
-    db.session.commit()
