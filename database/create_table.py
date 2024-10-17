a293b167375bba340f70060a471af44fc05d3046
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-10-17 13:58:09 +0300

    SqlLite

---
+++ database/create_table.py
@@ -0,0 +1,8 @@
+from To_Do import app, db
+from To_Do.models import User, TaskCollaborator, Task, Note
+
+# Create an application context
+with app.app_context():
+    # Create all tables
+    db.create_all()
+    db.session.commit()
