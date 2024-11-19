8f914c1079715ab87506d4b369b50170a06957be
Mohamed-Mostafaaa <momostafa110022@gmail.com>
2024-11-19 18:52:06 +0200

    fix small bug on logout feature

--- app.py
+++ app.py
@@ -7,8 +7,4 @@ app = create_app()
 
 if __name__ == "__main__":
     create_database()
-<<<<<<< HEAD:run.py
-    app.run(debug=True, host="0.0.0.0")
-=======
     app.run(debug=True, host='0.0.0.0')
->>>>>>> f4752c7298bea63e65ecac5c8e7fe6c26f175b2e:app.py
